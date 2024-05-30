d33559b643f5a33dc816304b8f6c0949e17031b1
Restyx <alekakst6@gmail.com>
2024-05-30 21:59:41 +0300

    added async

--- routes/users/createUser.mjs
+++ routes/users/createUser.mjs
@@ -1,27 +1,25 @@
-import { createUser } from '../../db-data.mjs';
+import { createUser } from "../../db-data.mjs";
 
 export default async (req, res) => {
-    let body = '';
- 
-    req.on('data', buffer => {
-        body += buffer.toString();
-    });
-    
-    req.on('end', () => {
-        const parsedBody = JSON.parse(body);
-        const name = parsedBody.name;
-        const age = parsedBody.age;
+  let body = "";
 
-        if (name && age) {
-            const user = { name, age: parseInt(age) };
-            createUser(user)
-            .then((result) => {
-                res.writeHead(200);
-                res.end();   
-            });
-            } else {
-            res.writeHead(400);
-            res.end({ message: 'Name and Age are required' })
-        }
-    });
+  req.on("data", (buffer) => {
+    body += buffer.toString();
+  });
+
+  req.on("end", async () => {
+    const parsedBody = JSON.parse(body);
+    const name = parsedBody.name;
+    const age = parsedBody.age;
+
+    if (name && age) {
+      const user = { name, age: parseInt(age) };
+      await createUser(user);
+      res.writeHead(200);
+      res.end();
+    } else {
+      res.writeHead(400);
+      res.end({ message: "Name and Age are required" });
+    }
+  });
 };
