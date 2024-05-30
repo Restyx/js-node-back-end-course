ef31364074ebbba36054fc0593d3258949d774bf
Restyx <alekakst6@gmail.com>
2024-05-30 21:22:31 +0300

    added some basic error handling

--- db-data.mjs
+++ db-data.mjs
@@ -10,49 +10,65 @@ export async function getUsers() {
 }
 
 export async function createUser(user) {
-  const query = {
-    name: "create-user",
-    text: "INSERT INTO users(name, age) VALUES($1, $2);",
-    values: [user.name, user.age],
-  };
+  try {
+    const query = {
+      name: "create-user",
+      text: "INSERT INTO users(name, age) VALUES($1, $2);",
+      values: [user.name, user.age],
+    };
 
-  const result = await pool.query(query);
-  return result.rowCount > 0;
+    const result = await pool.query(query);
+    return result.rowCount > 0;
+  } catch (error) {
+    console.log("Error while executing query:", error);
+  }
 }
 
 export async function getUserById(userId) {
-  const query = {
-    name: "get-user",
-    text: "SELECT id, name, age FROM users WHERE id = $1;",
-    values: [userId],
-  };
+  try {
+    const query = {
+      name: "get-user",
+      text: "SELECT id, name, age FROM users WHERE id = $1;",
+      values: [userId],
+    };
 
-  const result = await pool.query(query);
-  return result.rows[0];
+    const result = await pool.query(query);
+    return result.rows[0];
+  } catch (error) {
+    console.log("Error while executing query:", error);
+  }
 }
 
 export async function updateUser(userId, updatedData) {
-  let parsedValues = [];
-  for (const [key, value] of Object.entries(updatedData)) {
-    parsedValues.push(`${key} = ${key === 'age' ? value : `'${value}'`}`)
+  try {
+    let parsedValues = [];
+    for (const [key, value] of Object.entries(updatedData)) {
+      parsedValues.push(`${key} = ${key === "age" ? value : `'${value}'`}`);
+    }
+
+    const query = {
+      name: "update-user",
+      text: `UPDATE users SET ${parsedValues.toString()} WHERE id = ${userId};`,
+    };
+
+    const result = await pool.query(query);
+    return result.rowCount > 0;
+  } catch (error) {
+    console.log("Error while executing query:", error);
   }
-
-  const query = {
-    name: "update-user",
-    text: `UPDATE users SET ${parsedValues.toString()} WHERE id = ${userId}`,
-  };
-
-  const result = await pool.query(query);
-  return result.rowCount > 0;
 }
 
 export async function deleteUser(userId) {
-  const query = {
-    name: "delete-user",
-    text: "DELETE FROM users WHERE id = $1;",
-    values: [userId],
-  };
+  try {
+    const query = {
+      name: "delete-user",
+      text: "DELETE FROM users WHERE id = $1;",
+      values: [userId],
+    };
 
-  const result = await pool.query(query);
-  return result.rowCount > 0;
+    const result = await pool.query(query);
+    return result.rowCount > 0;
+  } catch (error) {
+    console.log("Error while executing query:", error);
+  }
 }
